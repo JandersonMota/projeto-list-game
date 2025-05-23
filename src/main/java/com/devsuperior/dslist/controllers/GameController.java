ee352bfe9640fda99efd08d0b1342b2083e581d8
JandersonMota00 <20231tadssaj@ifba.edu.br>
2025-05-23 11:09:10 -0300

    Faltou injetar (@Autowired) a dependência GameService no seu GameController

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.devsuperior.dslist.services.GameService;
 @RequestMapping(value = "/games")
 public class GameController {
 	
+	@Autowired
 	private GameService gameService;
 	
 	@GetMapping
